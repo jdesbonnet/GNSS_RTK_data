c44d12bddeb257a7bdcc743c5f4592a2f90643c5
Joe Desbonnet <jdesbonnet@gmail.com>
2018-10-10 02:43:47 +0100

    UBX raw data from M8P in RTK mode

--- 20181009/process.sh
+++ 20181009/process.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-rm *.obs *.nav *.pos
-convbin -od -os -oi -ot -f 1  base-20181009-1220.ubx
-convbin -od -os -oi -ot -f 1  rover-20181009-1221.ubx
-rnx2rtkp rover-20181009-1221.obs  base-20181009-1220.obs  base-20181009-1220.nav  > t.pos
+rm *.obs *.nav *.pos base.ubx rover.ubx
+gunzip -c base-20181009-1220.ubx.gz > base.ubx
+gunzip -c rover-20181009-1221.ubx.gz > rover.ubx
+convbin -od -os -oi -ot -f 1  base.ubx
+convbin -od -os -oi -ot -f 1  rover.ubx
+rnx2rtkp rover.obs  base.obs  base.nav  > rover.pos
 
