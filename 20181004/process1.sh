2f9815ac28e0332b600e0a9b92356b0da654c3de
Joe Desbonnet <jdesbonnet@gmail.com>
2018-10-10 23:28:42 +0100

    comments to process scripts

--- 20181004/process1.sh
+++ 20181004/process1.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Test conducted on grounds of National University of Ireland, Galway
+# on 4 Oct 2018. This is a static rover test (no movement).
+# Tested with RTKlib v2.4.3 b8
+# Base .ubx file is a mix of uBlox binary (UBX) and RTCM3 messages
+# Rover .ubx file is a mix of uBlox binary (UBX) and NMEA0183
+#
 rm *.obs *.nav *.pos base?.ubx rover?.ubx
 gunzip -c base-20181004-1236.ubx.gz > base1.ubx
 gunzip -c rover-20181004-1237.ubx.gz > rover1.ubx
--- 20181004/process2.sh
+++ 20181004/process2.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Test conducted on grounds of National University of Ireland, Galway
+# on 4 Oct 2018. In this test the rover moves.
+# Tested with RTKlib v2.4.3 b8
+# Base .ubx file is a mix of uBlox binary (UBX) and RTCM3 messages
+# Rover .ubx file is a mix of uBlox binary (UBX) and NMEA0183
+#
 rm *.obs *.nav *.pos base?.ubx rover?.ubx
 gunzip -c base-20181004-1452.ubx.gz > base2.ubx
 gunzip -c rover-20181004-1532.ubx.gz > rover2.ubx
