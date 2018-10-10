7beb320e84b5f9bac21cf1d23fa15c12b8979fae
Joe Desbonnet <jdesbonnet@gmail.com>
2018-10-10 23:26:37 +0100

    no comment

--- 20181009/process.sh
+++ 20181009/process.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# Test run around Tur Uisce, Doughiska, Galway, Ireland.
+# on 9 Oct 2018.
+# Tested with RTKlib v2.4.3 b8
+# Base .ubx file is a mix of uBlox binary (UBX) and RTCM3 messages
+# Rover .ubx file is a mix of uBlox binary (UBX) and NMEA0183
+#
 rm *.obs *.nav *.pos base.ubx rover.ubx
 gunzip -c base-20181009-1220.ubx.gz > base.ubx
 gunzip -c rover-20181009-1221.ubx.gz > rover.ubx
